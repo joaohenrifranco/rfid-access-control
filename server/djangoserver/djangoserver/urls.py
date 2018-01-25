acd67629b7d5292f1068c0542fca010e461d6292
joaohenriquef <joaohenrique@laspi.ufrj.br>
2018-01-25 10:08:28 -0200

    Make server respond to requests

--- server/djangoserver/djangoserver/urls.py
+++ server/djangoserver/djangoserver/urls.py
@@ -1,4 +1,4 @@
-"""arduinoserver URL Configuration
+"""djangoserver URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import include, path
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
