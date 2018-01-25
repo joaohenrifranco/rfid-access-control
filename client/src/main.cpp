f236b68397ceadeb4e20abfd71fb72dd3beba4d1
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2018-01-25 07:11:57 -0200

    Added infrastracture for ethernet client

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,6 +1,6 @@
 /*
  *
- *  This code is for Arduino MEGA 2560, for testing purposes;
+ *  This code is being compiled for Arduino Pro Mini 16MHz
  *
  */
 
@@ -9,11 +9,16 @@
  */
 #include <SPI.h>
 #include <MFRC522.h>
+#include <Ethernet.h>
 
 /*
  *  Constants
  */
 #define SERIAL_SPEED      9600
+#define MAC_ADDRESS       {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED}
+#define IP_ADDRESS        {192, 168, 88, 88}
+#define GATEWAY           {192, 168, 88, 1}
+#define SUBNET            {255, 255, 255, 0}
 #define BLACK             {0, 0, 0}       //Turn LED off
 #define BLUE              {0, 0, 255}
 #define GREEN             {0, 255, 0}
@@ -36,6 +41,17 @@
  *  Declaring the RFID module
  */
 MFRC522 rfid(SS_PIN, RST_PIN);
+
+/*
+ *  Declaring IP, MAC and the ethernet client itself
+ */
+IPAddress ip(IP_ADDRESS);
+#if defined(WIZ550io_WITH_MACADDRESS)
+;
+#else
+byte mac[] = MAC_ADDRESS;
+#endif
+EthernetClient ethClient;
 
 /*
  *  void WriteRGB (byte color[]);
@@ -85,9 +101,15 @@ String ReadRFIDTag ()
  */
 void setup() 
 {
+  // Starts serial communication for debugging purposes
   Serial.begin(SERIAL_SPEED);
+  // Starts SPI communication for devices
   SPI.begin();
+  // Initializes the RFID module
   rfid.PCD_Init();
+  // Initializes the Ethernet module
+  Ethernet.begin(mac, ip);
+  // Prints messages indicating that all setup has been done
   Serial.println("All set!");
   Serial.println("Please approach your card...");
   Serial.println();
