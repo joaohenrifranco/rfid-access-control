52afc200b17f6e62c93e675bdbabe412880f02e3
Gabriel  Gazola Milan <gabriel@laspi.ufrj.br>
2018-02-21 17:36:00 -0300

    Changed request data format

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -22,9 +22,11 @@
 //#define IP_ADDRESS        {192, 168, 88, 88}    //MY_OWN_IP
 //#define GATEWAY           {192, 168, 88, 1}
 //#define SUBNET            {255, 255, 255, 0}
-#define SERVER_IP         {192, 168, 88, 88}
-#define REQUEST_FROM      "192.168.88.88/x.php" //POST REQUEST_FROM
-#define REQUEST_PORT      80                    //Standard HTTP port
+#define SERVER_IP         {192, 168, 88, 61}
+#define REQUEST_UNLOCK    "http://192.168.88.61:8000/api/request-unlock"
+#define AUTHENTICATE      "http://192.168.88.61:8000/api/authenticate"
+#define AUTHORIZE_VISITOR "http://192.168.88.61:8000/api/authorize-visitor"
+#define REQUEST_PORT      8000                  //Standard HTTP port
 #define BLACK             {0, 0, 0}             //Turn LED off
 #define BLUE              {0, 0, 255}
 #define GREEN             {0, 255, 0}
@@ -43,14 +45,17 @@
  *  Pins
  */
 #define SS_PIN_IN         53
+#define SS_PIN_OUT        48
 #define RST_PIN_IN        49
-#define SS_PIN_OUT        255
-#define RST_PIN_OUT       255
-#define LED_R             47
-#define LED_G             45
-#define LED_B             43
-#define KEYPAD_LIN_PINS   {1, 2, 3, 4}
-#define KEYPAD_COL_PINS   {5, 6, 7}
+#define RST_PIN_OUT       47
+#define LED_IN_R          27
+#define LED_IN_G          29
+#define LED_IN_B          31
+#define LED_OUT_R         26
+#define LED_OUT_G         28
+#define LED_OUT_B         30
+#define KEYPAD_LIN_PINS   {33, 35, 37, 39}
+#define KEYPAD_COL_PINS   {41, 43, 45}
 
 /*
  *  Declaring the RFID modules
@@ -89,11 +94,20 @@ Keypad keyPad = Keypad(makeKeymap(keys), keyPadLinPins, keyPadColPins, KEYPAD_LI
  *  Returns:
  *  -
  */
-void WriteRGB (byte color[])
+void WriteRGB (byte color[], char in_out)
 {
-  byte pins[] = {LED_R, LED_G, LED_B};
-  for (byte i = 0; i < 3; i++)
-    digitalWrite(pins[i], color[i]);
+  if (in_out == 'i')
+  {
+    byte pins[] = {LED_IN_R, LED_IN_G, LED_IN_B};
+    for (byte i = 0; i < 3; i++)
+      digitalWrite(pins[i], color[i]);
+  }
+  else if (in_out == 'o')
+  {
+    byte pins[] = {LED_OUT_R, LED_OUT_G, LED_OUT_B};
+    for (byte i = 0; i < 3; i++)
+      digitalWrite(pins[i], color[i]);
+  }
 }
 
 /*
@@ -200,7 +214,7 @@ String HashedPassword (String password)
 }
 
 /*
- *  String GeneratePostData (String rfidTag, byte roomId, byte action);
+ *  String GenerateUnlockPostData (String rfidTag, byte roomId, byte action);
  *
  *  Description:
  *  - Generates a JSON format text to send through HTTP post
@@ -213,7 +227,7 @@ String HashedPassword (String password)
  *  Returns:
  *  [String] A JSON format text contatining the whole input data
  */
-String GeneratePostData (String rfidTag, byte roomId, byte action)
+String GenerateUnlockPostData (String rfidTag, byte roomId, byte action)
 {
   String aux = "{\n\t\"rfidTag\":\"";
   aux.concat(rfidTag);
@@ -222,6 +236,26 @@ String GeneratePostData (String rfidTag, byte roomId, byte action)
   aux.concat(",\n\t\"action\":");
   aux.concat(String(action));
   aux.concat("\n}");
+  return aux;
+}
+
+String GenerateAuthenticatePostData (String rfidTag, String password)
+{
+  String aux = "{\n\t\"rfidTag\":\"";
+  aux.concat(rfidTag);
+  aux.concat("\",\n\t\"password\":\"");
+  aux.concat(String(password));
+  aux.concat("\"\n}");
+  return aux;
+}
+
+String GenerateVisitorPostData (String rfidTag, String rfidTagVisitor)
+{
+  String aux = "{\n\t\"rfidTag\":\"";
+  aux.concat(rfidTag);
+  aux.concat("\",\n\t\"rfidTagVisitor\":\"");
+  aux.concat(String(rfidTagVisitor));
+  aux.concat("\"\n}");
   return aux;
 }
 
@@ -239,17 +273,19 @@ String GeneratePostData (String rfidTag, byte roomId, byte action)
  *  Returns:
  *  -
  */
-void SendPostRequest(String rfidTag, byte roomId, byte action)
+String SendPostRequest(String postData, String requestFrom)
 {
   String output = "";
-  String PostData = GeneratePostData(rfidTag, roomId, action);
   IPAddress serverIP(SERVER_IP);
   //IPAddress myIP(MY_OWN_IP);
-  uint16_t requestPort = REQUEST_PORT;
-  if (ethClient.connect(serverIP, requestPort))
+  int requestPort = REQUEST_PORT;
+  int connection = ethClient.connect("192.168.88.61", requestPort);
+  Serial.print("Connection output: ");
+  Serial.println(connection);
+  if (connection)
   {
     ethClient.print("POST ");
-    ethClient.print(REQUEST_FROM);
+    ethClient.print(requestFrom);
     ethClient.println(" HTTP/1.1");
     //ethClient.print("Host:  ");
     //ethClient.println(myIP);
@@ -257,16 +293,23 @@ void SendPostRequest(String rfidTag, byte roomId, byte action)
     ethClient.println("Connection: close");
     ethClient.println("Content-Type: application/json;");
     ethClient.print("Content-Length: ");
-    ethClient.println(PostData.length());
+    ethClient.println(postData.length());
     ethClient.println();
-    ethClient.println(PostData);
-    /*  Reading method (don't really know how to do it)
+    ethClient.println(postData);
+    Serial.println("Post sent!");
+    //  Reading method (don't really know how to do it)
+    Serial.println("Getting results...");
+    output = ethClient.readString();
+    Serial.print("Result: ");
+    Serial.println(output);
+    /*
     while (char c = ethClient.read())
     {
       output.concat(c);
     }
     */
   }
+  return output;
 }
 
 /*
@@ -293,13 +336,28 @@ void setup()
   SPI.begin();
   // Initializes the RFID module
   rfid_in.PCD_Init();
-  //rfid_out.PCD_Init();
+  rfid_out.PCD_Init();
   // Initializes the Ethernet module
-  // Ethernet.begin(mac);
+  Serial.println("Initializing Ethernet module...");
+  Ethernet.begin(mac);
+  Serial.print("My IP: ");
+  Serial.println(Ethernet.localIP());
+  Serial.println("Ok!");
   // Prints messages indicating that all setup has been done
   Serial.println("All set!");
   Serial.println();
   // <<WARNING>> Remember to dump all read characters from keypad before asking for password!
+
+
+
+  String postData = GenerateAuthenticatePostData ("12345", "senhasegura");
+  Serial.println(postData);
+  String output = SendPostRequest(postData, REQUEST_UNLOCK);
+  Serial.println(output);
+
+
+
+
 }
 
 /*
@@ -307,13 +365,12 @@ void setup()
  */
 void loop() 
 {
-  /*  Code for testing the keypad and RFID module
+  /*
   String tag = "";
   tag = ReadRFIDTag(rfid_in);
   while (tag == "")
   {
     Serial.println("Approach your card");
-    tag = ReadRFIDTag(rfid_in);
     delay(500);
   }
   Serial.print("Card tag: ");
@@ -321,14 +378,17 @@ void loop()
   String pw = GetPassword();
   Serial.print("Password: ");
   Serial.println(pw);
-  */
-  /*  Code for testing the SHA-256 hash function
-  String to_hash = "5077";
-  String hashed = HashedPassword(to_hash);
+  String hashed = HashedPassword(pw);
+  Serial.print("Hashed: ");
   Serial.println(hashed);
-  delay (1000);
   */
+
+
+
   /*  Full code for Arduino Client (still in development) */
+  
+
+  /*
   // Reads tag until it's not NONE and check if the person is entering or leaving the room
   String tag = "";
   bool enteringRoom = false;
@@ -349,9 +409,7 @@ void loop()
   }
   // Generates POST data, send request to server and waits for response
   bool authorized = false;
-  /*
-   *  TODO
-   */
+  // *** TODO ***
   // If authorized, check room's access level
   if (authorized == true)
     if (ROOM_ACCESS_LEVEL >= 3)
@@ -370,9 +428,7 @@ void loop()
         {
           // If the password isn't NONE, generates POST data, send request to server and waits for response
           bool passwordAccepted = false;
-          /*
-           *  TODO
-           */
+          // *** TODO ***
           if (passwordAccepted == true)
           {
             // If the password is OK, unlocks the door
@@ -386,4 +442,5 @@ void loop()
       // If access level is lower or equal to 2, unlocks the door
       UnlockDoor();
     }
+    */
 }
