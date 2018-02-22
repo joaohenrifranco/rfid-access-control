4a600389541a7f9218fde0fe90664593fce88f1a
joaohenriquef <joaohenrique@laspi.ufrj.br>
2018-02-22 17:08:18 -0300

    Add test host

--- server/djangoserver/djangoserver/settings.py
+++ server/djangoserver/djangoserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q$8^fv%je8w@6ta832gsv=q9h0-@v$t3rkq!=$ttbp%b5)wmcq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.88.61', 'laspi-icp-01']
 
 
 # Application definition
