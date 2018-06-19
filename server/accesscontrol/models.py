bbe87e91c7aec0891328f805e919c68891646b69
joaohenriquef <joaohenriquef@poli.ufrj.br>
2018-06-19 16:40:50 -0300

    Remove incorrect max lenght from uid field

--- server/accesscontrol/models.py
+++ server/accesscontrol/models.py
@@ -17,7 +17,6 @@ ACCESS_LEVEL_CHOICES = (
 class RfidTag(models.Model):
   uid = models.CharField(
     verbose_name=_('RFID tag UID'), 
-    max_length=8, 
     default=None, 
     unique=True
     )
@@ -203,7 +202,6 @@ class Event(models.Model):
     verbose_name=_('api Module')
     )
   uid = models.CharField(
-    max_length=8, 
     default=None, 
     blank=True, 
     null=True,
